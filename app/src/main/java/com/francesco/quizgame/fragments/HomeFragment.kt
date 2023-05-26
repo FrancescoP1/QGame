f11310c1c092ef2596cefec404cd57f06b9f1a18
FrancescoP1 <francesco.petrovici@s.unibuc.ro>
2023-05-26 10:47:44 +0300

    added camera interaction with data persistance

--- app/src/main/java/com/francesco/quizgame/fragments/HomeFragment.kt
+++ app/src/main/java/com/francesco/quizgame/fragments/HomeFragment.kt
@@ -1,16 +1,31 @@
 package com.francesco.quizgame.fragments
 
+import android.R.attr.bitmap
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.francesco.quizgame.R
 import com.francesco.quizgame.activities.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import org.checkerframework.checker.nullness.qual.NonNull
+import java.io.ByteArrayOutputStream
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +42,16 @@ class HomeFragment : Fragment() {
     private var userEmail: String? = null
     private var param2: String? = null
     private lateinit var firebaseAuth: FirebaseAuth
+    private val imageCapture = 100
+    private lateinit var context: Context
+    private lateinit var profilePicture: ImageView
+    private lateinit var firebaseStorage: FirebaseStorage
+    private lateinit var firebaseStorageReference: StorageReference
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.context = context
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +69,9 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
         firebaseAuth = FirebaseAuth.getInstance()
+        firebaseStorage = FirebaseStorage.getInstance()
+        firebaseStorageReference = firebaseStorage.reference
+
 
         // sign out
         val signOutButton: Button = view.findViewById(R.id.signOut_button)
@@ -63,9 +91,65 @@ class HomeFragment : Fragment() {
             startActivity(chooser)
         }
 
+        val cameraButton: Button = view.findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, imageCapture)
+            } catch (exception: ActivityNotFoundException) {
+                Toast.makeText(this.context, "Error while starting camera app", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        profilePicture = view.findViewById(R.id.profile_picture)
+        downloadProfilePictureFromCloud()
+
         val textView: TextView = view.findViewById(R.id.textView)
         textView.text = userEmail
         return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == RESULT_OK) {
+            val imageCaptured = data?.extras?.get("data") as Bitmap
+            uploadPictureToCloud(imageCaptured)
+            profilePicture.setImageBitmap(imageCaptured)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun uploadPictureToCloud(image: Bitmap) {
+        val uuid = firebaseAuth.currentUser?.uid
+        val profilePictureRef = firebaseStorageReference.child("/profileImages/$uuid")
+
+        // convert image to byteArray
+        val imageByteOutputStream = ByteArrayOutputStream()
+        image.compress(Bitmap.CompressFormat.JPEG, 100, imageByteOutputStream)
+        val imageBytes = imageByteOutputStream.toByteArray()
+
+        val uploadTask = profilePictureRef.putBytes(imageBytes)
+        uploadTask.addOnSuccessListener{
+            Toast.makeText(this.context, "Image uploaded", Toast.LENGTH_SHORT).show()
+        }
+        uploadTask.addOnFailureListener {
+            Toast.makeText(this.context, "There was an error uploading your image", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun downloadProfilePictureFromCloud() {
+        val fileId = firebaseAuth.currentUser?.uid
+        val pathRef = firebaseStorageReference.child("profileImages/$fileId")
+        val imageBytes = pathRef.getBytes(1024*1024*10)
+        imageBytes.addOnSuccessListener { image ->
+            val bitmapImage =  BitmapFactory.decodeByteArray(image, 0, image.size)
+            profilePicture.setImageBitmap(bitmapImage)
+        }
+        imageBytes.addOnFailureListener{
+            Toast.makeText(this.context, "There was an error retrieving your profile picture", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     companion object {
